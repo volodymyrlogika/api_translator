32b8376a88240614b986b6d63923f56a3b6af07a
volodymyrlogika <volodymyrlogika@gmail.com>
2026-01-31 14:30:47 +0200

    додали HTML/CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputText = document.querySelector('#input');
+    const outputText = document.querySelector('#output');
+    const translateBtn = document.querySelector('#translateBtn')
+
+    async function translateText() {
+        
+    }    
+    
+});
