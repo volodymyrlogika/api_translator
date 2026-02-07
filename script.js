6cebb9651d0397df84707eb41b6436a9e0cc8230
volodymyrlogika <volodymyrlogika@gmail.com>
2026-02-07 13:59:53 +0200

    додали функцію перекладу по API

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const translateBtn = document.querySelector('#translateBtn')
 
     async function translateText() {
-        
+        const text = inputText.value.trim();
+        const safeText = encodeURIComponent(text);
+        const email = 'volodymyr.logika@gmail.com'
+        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        outputText.textContent = data.responseData.translatedText
     }    
     
+    translateBtn.addEventListener('click', translateText);
+
 });
