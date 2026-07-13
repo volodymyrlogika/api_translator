3ae0075297851d5faef8e7437443b71817ba989c
volodymyrlogika <volodymyr.logika@gmail.com>
2026-07-13 19:14:54 +0300

    Add language toggle functionality for translation

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.querySelector('#input');
     const outputText = document.querySelector('#output');
     const translateBtn = document.querySelector('#translateBtn')
+    let langpair = 'uk|en'
+
+    const changeBtn = document.querySelector('#changeBtn');
+    changeBtn.addEventListener('click', () => {
+        if (langpair === 'uk|en') {
+            langpair = 'en|uk';
+        } else {
+            langpair = 'uk|en';
+        }
+    });
 
     async function translateText() {
         const text = inputText.value.trim();
         const safeText = encodeURIComponent(text);
         const email = 'volodymyr.logika@gmail.com'
-        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
+        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=${langpair}&de=${email}`;
 
         const response = await fetch(url);
         const data = await response.json();
